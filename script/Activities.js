ad066acd13247198d9a8089bca7f8f25995357a7
MrHasib1 <hasibul5611@gmail.com>
2025-03-04 00:53:33 +0600

    all done assignment

--- script/Activities.js
+++ script/Activities.js
@@ -1,15 +1,14 @@
-// alert();
 function history(id, id2) {
   const container = document.getElementById(id);
   const p = document.createElement("p");
   p.className = "m-2 bg-gray-300 rounded-lg p-2";
   const text = document.getElementById(id2).textContent;
-
   const time = new Date();
-  const hour = String(time.getHours()).padStart(2, "0");
-  const minute = String(time.getMinutes()).padStart(2, "0");
-  const second = String(time.getSeconds()).padStart(2, "0");
-
-  p.innerText = `You have completed the task "${text}" at "${hour}" : "${minute}" : "${second}"`;
+  const n = time.toLocaleDateString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+  p.innerText = `You have completed the task ${text} at ${n} `;
   container.appendChild(p);
 }
